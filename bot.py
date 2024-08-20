7b0668a4338d7b5331ebbc3fdb0f2d62a4d6dc41
dende1896 <patrick.schiller@stroeer-publishing.de>
2024-08-20 15:11:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,13 @@
 import tweepy
 import os
 
-# Hol dir deine API-Keys aus den Umgebungsvariablen
-consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
-consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
-access_token = os.getenv('TWITTER_ACCESS_TOKEN')
-access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+# Set up the client with bearer token (v2 API)
+bearer_token = os.getenv('TWITTER_BEARER_TOKEN')
+client = tweepy.Client(bearer_token=bearer_token)
 
-# Überprüfen, ob die Variablen korrekt geladen wurden
-print(consumer_key)
-print(consumer_secret)
-print(access_token)
-print(access_token_secret)
-
-# Authentifizierung
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
-
-# Einen Tweet posten
+# Define the tweet text
 tweet = "Hello, world! #ArtikelRoaster"
-api.update_status(tweet)
+
+# Post the tweet using the v2 API
+response = client.create_tweet(text=tweet)
+print(response)
