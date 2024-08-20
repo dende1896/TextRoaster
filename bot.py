5d7689f84e333e84cdb7cae92bba4bcd3d68cf9d
dende1896 <patrick.schiller@stroeer-publishing.de>
2024-08-20 12:48:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,10 +7,16 @@ consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
 access_token = os.getenv('TWITTER_ACCESS_TOKEN')
 access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
+# Überprüfen, ob die Variablen korrekt geladen wurden
+print(consumer_key)
+print(consumer_secret)
+print(access_token)
+print(access_token_secret)
+
 # Authentifizierung
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Einen Tweet posten
-tweet = "Hello, world! #ArtikelRoaster"
+tweet = "Hello, world! #ArtikelRoaster
 api.update_status(tweet)
